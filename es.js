fe5f018fcb49e4265fcb784b335ea5ca4adeb47a
carlo-ddm <carloduminuco90@gmail.com>
2022-09-10 01:32:11 +0200

    inserisco audio calcolatrice

--- es.js
+++ es.js
@@ -1,14 +1,19 @@
+const audio = new Audio('mixkit-positive-interface-click-1112.wav')
+
 function n(dato){ 
+  audio.play()
   document.getElementById("operazioni").value+=dato;
   /** Prendo il display. Alla funzione viene passato l'id dell'input cliccato. Al value del display viene assegnato il valore dell'ID dell'input in concatenazione */ 
 }
    
 function operazione() { 
+  audio.play()
   document.getElementById("operazioni").value=eval(document.getElementById("operazioni").value); 
   /** Prendo il display. Alla funzione operazione(), con il metodo eval() i valori presenti vengono operati*/ 
 }
    
-function cancella() { 
+function cancella() {
+  audio.play() 
   document.getElementById("operazioni").value=""; 
 }
 
